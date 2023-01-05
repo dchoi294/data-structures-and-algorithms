64c1dba76b9f63fc26561761d3b323f67a620447
dchoi294 <dchoi294@gmail.com>
2023-01-04 20:17:20 -0800

    CC13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,33 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    square_stack = Stack()
+    curly_stack = Stack()
+    round_stack = Stack()
+    for i in string:
+        if i == "[":
+            square_stack.push(i)
+        if i == "]" and square_stack.is_empty():
+            return False
+        if i == "]" and not square_stack.is_empty():
+            square_stack.pop()
+
+        if i == "{":
+            curly_stack.push(i)
+        if i == "}" and curly_stack.is_empty():
+            return False
+        if i == "}" and not curly_stack.is_empty():
+            curly_stack.pop()
+
+        if i == "(":
+            round_stack.push(i)
+        if i == ")" and round_stack.is_empty():
+            return False
+        if i == ")" and not round_stack.is_empty():
+            round_stack.pop()
+
+    if square_stack.is_empty() and curly_stack.is_empty() and round_stack.is_empty():
+        return True
+
+    return False
