356626e2fc6e34bbb0a688f492552e3cd190a0ab
dchoi294 <dchoi294@gmail.com>
2023-01-30 18:04:24 -0800

    CC31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,12 @@
 from data_structures.hashtable import Hashtable
+import string
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(text):
+    words = [word.strip(string.punctuation).lower() for word in text.split()]
+    find = Hashtable()
+
+    for word in words:
+        if find.has(word):
+            return word
+        find.set(word, None)
